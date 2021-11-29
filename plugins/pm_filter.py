5b535a322eda438f85ea655840872d99cc1cfd38
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-29 19:36:52 +0530

    Update ______.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -489,6 +489,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
+    elif query.data == "whois":
+        buttons = [[
+            InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.WHOIS_TXT,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
     elif query.data == "fun":
         buttons = [[
             InlineKeyboardButton('👩‍🦯 Back', callback_data='help')
