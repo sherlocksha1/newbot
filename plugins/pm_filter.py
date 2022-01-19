2bd14ba8393d511dd7bc512f2ce658a6e950217e
Don <96370825+SflixBots@users.noreply.github.com>
2022-01-19 21:46:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -711,9 +711,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "pages":
         await query.answer()
     elif query.data == "start":
-    elif query.data == "pages":
-        await query.answer()
-    elif query.data == "start":
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
