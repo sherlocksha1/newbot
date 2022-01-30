86327a49223ba869ab4a20695968966a5ead9571
Don <96370825+SflixBots@users.noreply.github.com>
2022-01-30 08:50:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Adithyanadii/EvaMaria.git /EvaMaria
+  git clone https://git.heroku.com/luciferevamaria.git /luciferevamaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /luciferevamaria
 fi
-cd /EvaMaria
-pip3 install -U -r requirements.txt
+cd /luciferevamaria
+pip freeze > requirements.txt
 echo "Starting Bot...."
 python3 bot.py
