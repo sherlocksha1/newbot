5b23b300a88f9374035075a8f232e5d179236e7a
Don <96370825+SflixBots@users.noreply.github.com>
2022-01-31 12:15:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Adithyanadii/LS-BoTz.git /LS-BoTz
-cd /LS-BoTz
-echo "Installing Requirements..."
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /EvaMaria
+fi
+cd /EvaMaria
 pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+echo "Starting Bot...."
 python3 bot.py
