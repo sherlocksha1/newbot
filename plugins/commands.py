722637a0cbae70a7942604519bb7c1e5aa15eb08
sherlocksha1 <98747957+sherlocksha1@users.noreply.github.com>
2022-02-21 18:51:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -75,7 +75,7 @@ async def start(client, message):
             ]
         ]
 
-       if message.command[1] != "subscribe":
+        if message.command[1] != "subscribe":
             btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"checksub#{message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
